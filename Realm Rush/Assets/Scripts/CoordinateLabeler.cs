708b138109aeb107445323e6987e5172c50e9f85
Dudar2000 <semann7@gmail.com>
2024-06-20 02:47:08 +0300

    Project Created, Tiles an Grid created

---
+++ Realm Rush/Assets/Scripts/CoordinateLabeler.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Numerics;
+
+
+[ExecuteAlways]
+public class CoordinateLabeler : MonoBehaviour
+{
+   TextMeshPro label;
+   Vector2Int coordinates = new Vector2Int();
+
+
+   void Awake() 
+   {
+        label = GetComponent<TextMeshPro>();
+        DiplayCoordinates();
+   }
+    
+    void Update()
+    {
+        if(!Application.isPlaying)
+        {
+          DiplayCoordinates(); 
+          UpdateObjectName();
+        }
+    }
+
+
+    //Method for tile coordinates info
+    void DiplayCoordinates()
+    {   
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x) ;
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z) ;
+        label.text = coordinates.x + "," + coordinates.y;
+    }
+
+    void UpdateObjectName()
+    {
+        transform.parent.name = coordinates.ToString();
+    }
+
+}
